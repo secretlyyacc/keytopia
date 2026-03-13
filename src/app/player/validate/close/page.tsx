5ee07bf1b867f1e5517883001f208825d294e050
adwdawdawd <growlyakunsave@gmail.com>
2026-03-13 13:50:08 +0000

    Delete src directory

--- src/app/player/validate/close/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Close() {
-  return (
-    <script>
-      window.close();
-    </script>
-  );
-}
--- src/security/RequestSizeLimiter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const requestSizeLimiter = express.json({
-    limit: '10kb' // Batasi ukuran request JSON
-});
-
-module.exports = requestSizeLimiter;
